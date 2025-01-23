654ae9d93fb1e0274e2c00a79b924fc3e2c7c312
George Edwards <ge@speedwelshpool.com>
2025-01-23 14:49:39 +0000

    efficiencies and improvements

--- src/controllers/PalletforcePodProcessor.js
+++ src/controllers/PalletforcePodProcessor.js
@@ -1,63 +1,85 @@
-import File from '../entities/File.js';
-import ProcessingStrategy from '../entities/ProcessingStrategy.js';
+import File from "../entities/File.js";
+import ProcessingStrategy from "../entities/ProcessingStrategy.js";
+import FileRepositoryInterface from "../services/FileRepositoryInterface.js";
+import ImageProcessor from "../services/ImageProcessor.js";
+import FtpClient from "../services/FtpClient.js";
 
 export default class PalletforcePodProcessor {
-    constructor(ftpClient, fileRepository, imageProcessor) {
-        this.ftpClient = ftpClient;
-        this.imageProcessor = imageProcessor;
-        this.fileRepository = fileRepository;
+  /**
+   * @property {FtpClient} ftpClient the ftp client
+   */
+  ftpClient;
+
+  /**
+   *
+   * @param {FtpClient} ftpClient the ftp client
+   * @param {FileRepositoryInterface} fileRepository the file repository implementation we are using
+   * @param {ImageProcessor} imageProcessor the image processor implementation we are using e.g. Sharp for Tiffs
+   */
+  constructor(ftpClient, fileRepository, imageProcessor) {
+    this.ftpClient = ftpClient;
+    this.imageProcessor = imageProcessor;
+    this.fileRepository = fileRepository;
+  }
+
+  async processTiffFiles(inFolderPath, archiveFolderPath) {
+    console.log("Palletforce processing started...");
+    console.time("palletforce-processing-time");
+
+    const files = await this.fileRepository.dir(inFolderPath);
+    const validExtensions = ["tiff", "tif"];
+
+    await this.ftpClient.connect();
+
+    for (const fileName of files) {
+      const ext = fileName.split(".").pop().toLowerCase();
+
+      if (!validExtensions.includes(ext)) {
+        continue;
+      }
+
+      console.log("Processing file:", fileName);
+
+      const fileContents = await this.fileRepository.read(
+        inFolderPath,
+        fileName
+      );
+
+      // Create a new File instance representing the raw, unprocessed image file
+      const _file = new File(inFolderPath, fileName, fileContents);
+
+      // Create our processing strategy representing the desired output format
+      const processingStrategy = new ProcessingStrategy({
+        colour: "blackandwhite",
+        format: "tiff",
+        compression: "lzw",
+      });
+
+      // Create a new File instance representing the processed image file
+      // setting it's content to the processed image data
+      const _outputFile = new File(
+        archiveFolderPath,
+        `processed_${_file.name}`,
+        await this.imageProcessor.process(_file, processingStrategy)
+      );
+
+      // Write the processed image file to the archive folder
+      await this.fileRepository.write(
+        archiveFolderPath,
+        _outputFile.name,
+        _outputFile.content
+      );
+
+      // Upload the processed image file to the FTP server
+      await this.ftpClient.upload(
+        _outputFile.fullpath(),
+        `/PWImages/InDel/${_outputFile.name}`
+      );
+
+      // Delete the original image file
+      await this.fileRepository.delete(_file.path, _file.name);
     }
 
-    async processTiffFiles(inFolderPath, archiveFolderPath) {
-
-        const files = await this.fileRepository.dir(inFolderPath);
-        const validExtensions = ['tiff', 'tif'];
-
-        for (const fileName of files) {
-
-            console.log("Processing file:", fileName);
-
-            const ext = fileName.split('.').pop().toLowerCase();
-
-            if (!validExtensions.includes(ext)) {
-                continue;
-            }
-
-            const fileContents = await this.fileRepository.read(inFolderPath, fileName);
-
-            // Create a new File instance representing the raw, unprocessed image file
-            const _file = new File(
-                inFolderPath,
-                fileName,
-                fileContents
-            );
-
-            // Create our processing strategy representing the desired output format
-            const processingStrategy = new ProcessingStrategy({
-                colour: 'blackandwhite',
-                format: 'tiff',
-                compression: 'lzw'
-            });
-
-            // Create a new File instance representing the processed image file
-            // setting it's content to the processed image data
-            const _outputFile = new File(
-                archiveFolderPath,
-                `processed_${_file.name}`,
-                await this.imageProcessor.process(_file, processingStrategy)
-            );
-
-            // Write the processed image file to the archive folder
-            await this.fileRepository.write(archiveFolderPath, _outputFile.name, _outputFile.content);
-
-            // Upload the processed image file to the FTP server
-            await this.ftpClient.connect();
-            await this.ftpClient.upload(_outputFile.fullpath(), `/PWImages/InDel/${_outputFile.name}`);
-
-            // Delete the original image file
-            await this.fileRepository.delete(_file.path, _file.name);
-
-        }
-
-    }
+    console.timeEnd("palletforce-processing-time");
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,50 +1,54 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import PalletforcePodProcessor from './controllers/PalletforcePodProcessor.js';
-import HazchemPodProcessor from './controllers/HazchemPodProcessor.js';
-import FtpClient from './services/FtpClient.js';
-import ImageProcessor from './services/ImageProcessor.js';
-import FileRepository from './services/FileRepository.js';
+import PalletforcePodProcessor from "./controllers/PalletforcePodProcessor.js";
+import HazchemPodProcessor from "./controllers/HazchemPodProcessor.js";
+import FtpClient from "./services/FtpClient.js";
+import ImageProcessor from "./services/ImageProcessor.js";
+import FileRepository from "./services/FileRepository.js";
 
 try {
-    const palletforceFtpClient = new FtpClient({
-        host: process.env.PF_HOST,
-        user: process.env.PF_USER,
-        password: process.env.PF_PASSWORD,
-        secure: true
-    });
+  const palletforceFtpClient = new FtpClient({
+    host: process.env.PF_HOST,
+    user: process.env.PF_USER,
+    password: process.env.PF_PASSWORD,
+    secure: true,
+  });
 
-    const fileRepository = new FileRepository();
-    const imageProcessor = new ImageProcessor();
+  const fileRepository = new FileRepository();
+  const imageProcessor = new ImageProcessor();
 
-    const pfProcessor = new PalletforcePodProcessor(
-        palletforceFtpClient,
-        fileRepository,
-        imageProcessor,
-    );
+  const pfProcessor = new PalletforcePodProcessor(
+    palletforceFtpClient,
+    fileRepository,
+    imageProcessor
+  );
 
-    pfProcessor.processTiffFiles(process.env.PF_INPUT_FOLDER, process.env.PF_ARCHIVE_FOLDER);
+  await pfProcessor.processTiffFiles(
+    process.env.PF_INPUT_FOLDER,
+    process.env.PF_ARCHIVE_FOLDER
+  );
 
-    const hazchemFtp = new FtpClient({
-        host: process.env.HAZ_HOST,
-        user: process.env.HAZ_USER,
-        password: process.env.HAZ_PASSWORD,
-        secure: false
-    });
+  const hazchemFtp = new FtpClient({
+    host: process.env.HAZ_HOST,
+    user: process.env.HAZ_USER,
+    password: process.env.HAZ_PASSWORD,
+    secure: false,
+  });
 
-    const hazProcessor = new HazchemPodProcessor(
-        hazchemFtp,
-        fileRepository,
-        imageProcessor,
-    );
+  const hazProcessor = new HazchemPodProcessor(
+    hazchemFtp,
+    fileRepository,
+    imageProcessor
+  );
 
-    hazProcessor.processTiffFiles(process.env.HAZ_INPUT_FOLDER, process.env.HAZ_ARCHIVE_FOLDER);
+  await hazProcessor.processTiffFiles(
+    process.env.HAZ_INPUT_FOLDER,
+    process.env.HAZ_ARCHIVE_FOLDER
+  );
 
-
-
-
+  console.log("Finished");
 } catch (error) {
-    console.error(error, error.message);
-    process.exit(1);
+  console.error(error, error.message);
+  process.exit(1);
 }
